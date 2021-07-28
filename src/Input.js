3e8e7c4a0c3a6ae8b6e238e812b8fa97dcd75476
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-28 00:40:50 -0500

    Added CSS

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,6 @@
 const Input = ({ colorValue, setColorValue }) => {
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form className="inputForm" onSubmit={(e) => e.preventDefault()}>
       <label>Add Color Name:</label>
       <input
         autoFocus
